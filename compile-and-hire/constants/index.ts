1e312c34534e21f8f75e4a67c8833ad04d8a21ba
thomasbulll <tombull66@hotmail.com>
2024-03-30 12:45:31 +0000

    Add basic footer

---
+++ compile-and-hire/constants/index.ts
@@ -0,0 +1,18 @@
+export const footerLinks = [
+    {
+      title: "About",
+      links: [
+        { title: "How it works", url: "/" },
+        { title: "Featured", url: "/" },
+        { title: "Bussiness Relation", url: "/" },
+      ],
+    },
+    {
+      title: "Socials",
+      links: [
+        { title: "Instagram", url: "/" },
+        { title: "Twitter", url: "/" },
+        { title: "Facebook", url: "/" },
+      ],
+    },
+  ];
