dfdb96b5833a51c9ce4b557fec21bcc3b34c7577
thomasbulll <tombull66@hotmail.com>
2024-03-30 12:33:32 +0000

    Add basic nav bar

--- compile-and-hire/app/layout.tsx
+++ compile-and-hire/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "Compile and Hire",
@@ -16,7 +15,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="relative">
+        <NavBar />
+        {children} 
+        <Footer />
+      </body>
     </html>
   );
 }
--- compile-and-hire/components/CustomButton.tsx
+++ compile-and-hire/components/CustomButton.tsx
@@ -3,11 +3,12 @@
 import { CustomButtonProps } from '@/types';
 import React from 'react'
 
-const CustomButton = ({title, containerStyles, handleClick}: CustomButtonProps) => {
+const CustomButton = ({title, containerStyles,
+    handleClick, btnType}: CustomButtonProps) => {
   return (
     <button
     disabled={false}
-    type={"button"}
+    type={btnType || "button"}
     className={`custom-btn ${containerStyles}`}
     onClick={handleClick}
     >
