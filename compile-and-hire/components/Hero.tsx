a6a0cf42b8b91dc4f4f8175a4a034b0e7f5680ca
thomasbulll <tombull66@hotmail.com>
2024-04-02 23:12:38 +0100

    Fix links

--- compile-and-hire/components/Hero.tsx
+++ compile-and-hire/components/Hero.tsx
@@ -3,15 +3,13 @@
 import React from 'react'
 import Image from 'next/image';
 import CustomButton from './CustomButton';
-import { redirect } from 'next/navigation';
+import Link from "next/link";
 
 const Hero = () => {
     const handleScroll = () => {
 
     }
-    const handleNavigation = (str: string) => {
-        return redirect(str);
-    }
+
   return (
     <div className="hero">
         <div className="flex-1 pt-36 padding-x">
@@ -22,16 +20,20 @@ const Hero = () => {
                 Discover opportunitues, build projects, and launch your career.
             </p>
             <div className="flex space-x-2">
-            <CustomButton
+            <Link href={"/login"}>
+                <CustomButton
                 title="Sign in"
                 containerStyles="bg-gray-100 text-black rounded-full mt-10 hover:bg-gray-200"
                 handleClick={handleScroll}
-            />
-            <CustomButton
-                title="Sign Up"
-                containerStyles="bg-blue-600 text-white rounded-full mt-10 hover:bg-blue-700"
-                handleClick={handleScroll}
-            />
+                />
+            </Link>
+            <Link href={"/signup"}>
+                <CustomButton
+                    title="Sign Up"
+                    containerStyles="bg-blue-600 text-white rounded-full mt-10 hover:bg-blue-700"
+                    handleClick={handleScroll}
+                />
+            </Link>
             </div>
         </div>
         <div className='hero__image-container'>
--- compile-and-hire/components/NavBar.tsx
+++ compile-and-hire/components/NavBar.tsx
@@ -9,15 +9,16 @@ const NavBar = () => {
   return (
     <header className="w-full absolute z-10">
         <nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent'>
-            <Link href="" className='flex justify-center items-center'>
+            <Link href="/" className='flex justify-center items-center'>
                 Home
             </Link>
-
-            <CustomButton
-                title='Sign in'
-                btnType='button'
-                containerStyles='bg-gray-100 text-primary-blue rounded-full bg-white min-w-[130px] hover:bg-gray-200'
-            />
+            <Link href="/login" >
+              <CustomButton
+                  title='Sign in'
+                  btnType='button'
+                  containerStyles='bg-gray-100 text-primary-blue rounded-full bg-white min-w-[130px] hover:bg-gray-200'
+              />
+            </Link>
         </nav>
     </header>
   )
