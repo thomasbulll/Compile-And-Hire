5c82845c931f9a29093238d4f52950ca3cd52205
thomasbulll <tombull66@hotmail.com>
2024-04-08 22:03:42 +0100

    Create user db logic

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,10 @@
 "use server";
 
+import bcrypt from "bcrypt";
 import * as zod from "zod";
 import { RegisterSchema } from "@/schemas";
+
+import { db } from "@/lib/db";
 
 export const register = async (values: zod.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
@@ -10,7 +13,44 @@ export const register = async (values: zod.infer<typeof RegisterSchema>) => {
         return {error: "Invalid fields!"};
     }
 
+    const { email, password, name } = validatedFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // const existingUserEmail = await db.user.findUnique({
+    //     where:{
+    //         email,
+    //     }
+    // });
+
+    // const existingUsername = await db.user.findUnique({
+    //     where:{
+    //         name,
+    //     }
+    // });
+
+    // if (existingUserEmail){
+    //     return {
+    //         error: "Email already in use!"
+    //     };
+    // }
+
+    // if (existingUsername){
+    //     return {
+    //         error: "Username already in use!"
+    //     };
+    // }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+        }
+    })
+
+    //TODO: Send verification email
+
     return {
-        success: "Email Sent!"
+        success: "Account Created"
     }
 }
