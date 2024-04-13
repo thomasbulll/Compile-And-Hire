0db5993462a803ef9313670ac4e40cc58b541d71
thomasbulll <tombull66@hotmail.com>
2024-04-13 13:35:40 +0100

    Add basic signout

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,12 +1,23 @@
-import {auth} from "@/auth";
+import {auth, signOut} from "@/auth";
 
 const SettingsPage = async () => {
     const session = await auth();
     return (
-        <main className="main">
+        <main className="main pt-36">
         <h1>
             {JSON.stringify(session)}
         </h1>
+        <form
+        action={
+            async () => {
+                "use server";
+
+                await signOut();
+            }}>
+            <button type="submit">
+                Signout
+            </button>
+        </form>
         </main>
     );
 }
