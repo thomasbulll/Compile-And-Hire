a4cd510be1e1170fb217e1efc5cc1992cb830c2f
thomasbulll <tombull66@hotmail.com>
2024-04-13 14:57:12 +0100

    Goog + Github Signin

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -2,10 +2,16 @@
 
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
-
+import { signIn } from "next-auth/react";
 import { Button } from "@/components/ui/button";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 export const Social = () => {
+  const onClick = (provider: "google" | "github") => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT
+    })
+  }
 
   return (
     <div className="flex items-center w-full gap-x-2">
@@ -13,7 +19,7 @@ export const Social = () => {
         size="lg"
         className="w-full"
         variant="outline"
-        onClick={() => {}}
+        onClick={() => onClick("google")}
       >
         <FcGoogle className="h-5 w-5" />
       </Button>
@@ -21,7 +27,7 @@ export const Social = () => {
         size="lg"
         className="w-full"
         variant="outline"
-        onClick={() => {}}
+        onClick={() => onClick("github")}
       >
         <FaGithub className="h-5 w-5" />
       </Button>
