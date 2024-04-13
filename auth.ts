eaba0d1b25f550552f78b9eb8db66c35093180bd
thomasbulll <tombull66@hotmail.com>
2024-04-13 14:25:45 +0100

    Session Tokens

--- auth.ts
+++ auth.ts
@@ -1,7 +1,10 @@
-import NextAuth from "next-auth"
-import authConfig from '@/auth.config';
-import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { UserRole } from "@prisma/client";
+import { getUserById } from "@/data/user";
 import { db } from "@/lib/db";
+
+import NextAuth from "next-auth";
+import authConfig from '@/auth.config';
 
 export const {
   handlers: { GET, POST },
@@ -15,10 +18,28 @@ export const {
       if (token.sub && session.user) {
         session.user.id = token.sub;
       }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+      console.log(token.role)
       console.log({sessionToken: token})
       return session
     },
     async jwt({token}) {
+      // Not logged in
+      if (!token.sub){
+        return token;
+      }
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser){
+        return token;
+      }
+
+      token.role = existingUser.role;
+
       return token
     }
   },
