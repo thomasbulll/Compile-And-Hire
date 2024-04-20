e7d353c11170f91f4567ec1060460c56e5827b57
thomasbulll <tombull66@hotmail.com>
2024-04-20 10:15:04 +0100

    Add verification form logic

--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -1,10 +1,40 @@
 "use client";
 
 import { CardWrapper } from "@/components/auth/card-wrapper";
+import { newVerification } from "@/actions/new-verification";
+import { FormError } from "@/components/form-error";
+import { FormSuccess } from "@/components/form-success";
 
+import { useSearchParams } from "next/navigation";
+import { useCallback, useEffect, useState } from "react";
 import { PulseLoader } from "react-spinners";
 
 export const NewVerificationFom = () => {
+    const [error, setError] = useState<string | undefined>();
+    const [success, setSuccess] = useState<string | undefined>();
+
+    const searchParams = useSearchParams();
+
+    const token = searchParams.get("token")
+
+    const onSubmit = useCallback(() => {
+        if (token) {
+            newVerification(token).then((data) => {
+                setSuccess(data.success)
+                setError(data.error)
+            })
+            .catch(() => {
+                setError("Something went wrong.")
+            });
+        }else{
+            setError("Missing Token")
+        }
+    }, [token]);
+
+    useEffect(() => {
+        onSubmit();
+    }, [onSubmit])
+
     return (
         <CardWrapper
         headerLabel="Confirm your verification"
@@ -12,7 +42,11 @@ export const NewVerificationFom = () => {
         backButtonHref="/auth/login"
         backButtonLabel="Back to login">
             <div className="flex items-center w-full justify-center">
-                <PulseLoader/>
+                {!success && !error &&(
+                    <PulseLoader/>
+                )}
+                <FormError message={error}/>
+                <FormSuccess message={success}/>
             </div>
         </CardWrapper>
     )
