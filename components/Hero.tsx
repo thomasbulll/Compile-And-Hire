b297f518a56a41f9de2717298e5e9ea8b7c2bfe8
thomasbulll <tombull66@hotmail.com>
2024-04-25 22:21:49 +0100

    Add basic images to choose register type

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -28,7 +28,7 @@ const Hero = () => {
                 handleClick={handleScroll}
                 />
             </Link>
-            <Link href={"/auth/student-register"}>
+            <Link href={"/auth/choose-register-type"}>
                 <CustomButton
                     title="Sign Up"
                     containerStyles="bg-blue-600 text-white rounded-full mt-10 hover:bg-blue-700"
--- components/auth/choose-register-type-form.tsx
+++ components/auth/choose-register-type-form.tsx
@@ -1,5 +1,6 @@
 import {LargeCardWrapper} from "@/components/auth/large-card-wrapper";
 import Link from "next/link";
+import Image from 'next/image';
 
 export const ChooseRegisterTypeForm = () => {
     return (
@@ -9,17 +10,35 @@ export const ChooseRegisterTypeForm = () => {
         backButtonLabel="Back to login"
         headerTitle="Register">
             <div className="w-full flex justify-center items-center">
-                <div>
-                    <Link
-                    href="/auth/business-register">
-                        Business
-                    </Link>
-                </div>
-                <div>
-                    <Link
-                        href="/auth/student-register">
-                        Student
-                    </Link>
+                <div className="flex justify-center">
+                    <div className="mr-4 items-center border-0">
+                        <Link href="/auth/business-register">
+                        <a>Business</a>
+                        <div className="pr-50">
+                            <Image
+                                width="250"
+                                height = "250"
+                                src="/cartoon-office.jpg"
+                                alt="hero"
+                                className='object-contain'
+                            />
+                        </div>
+                        </Link>
+                    </div>
+                    <div className="border-0">
+                        <Link href="/auth/student-register">
+                        <a>Student</a>
+                        <div>
+                            <Image
+                                width="250"
+                                height = "250"
+                                src="/cartoon-graduation-hat.jpg"
+                                alt="hero"
+                                className='object-contain'
+                            />
+                        </div>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </LargeCardWrapper>
