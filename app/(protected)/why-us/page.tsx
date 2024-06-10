6919dde412f690bf66a7cada003aff174c0483e9
Thomas Bull <tombull66@hotmail.com>
2024-06-10 17:28:13 +0100

    Add a basic about and why us page

--- app/(protected)/why-us/page.tsx
+++ app/(protected)/why-us/page.tsx
@@ -1,41 +1,69 @@
-const WhyUsPage = async () => {
+import Link from "next/link";
+
+const WhyUsPage = () => {
 
     return (
         <div className="space-y-6">
             <section className="container mx-auto py-20">
-                <h2 className="text-4xl font-bold text-center mb-8">About Compile & Hire: Bridging the Gap Between Talent and Opportunity</h2>
+                <h2 className="text-3xl font-bold text-center mb-8">Why Choose Compile & Hire?</h2>
 
-                <div className="prose text-gray-700 mb-8">
-                    <h3 className="text-1xl font-semibold text-center mb-8">Compile & Hire is your one-stop platform for connecting aspiring computer science students and software engineers with businesses seeking fresh talent for their projects. We understand the challenges both sides face:</h3>
-                    <ul className="list-disc space-y-2">
-                    <li>Students and aspiring engineers: Finding real-world experience to build your resume and showcase your skills can be tough. Many entry-level positions require experience, creating a frustrating catch-22.</li>
-                    <li>Businesses: Hiring qualified developers can be expensive and time-consuming. You might struggle to find the perfect fit, especially for smaller projects.</li>
-                    </ul>
-                </div>
-
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-10 text-center pt-5">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
                     <div>
                     <h3 className="text-2xl font-semibold mb-4">For Students and Aspiring Engineers</h3>
-                    <ul className="list-disc space-y-2 list-inside ">
-                        <li className="text-gray-700">Gain valuable experience on real-world projects.</li>
-                        <li className="text-gray-700">Get mentored by experienced professionals.</li>
-                        <li className="text-gray-700">Boost your resume with project experience.</li>
+                    <ul className="list-disc space-y-2">
+                        <li className="text-gray-700">
+                            <strong>Launch Your Career Faster: </strong> 
+                            Break the entry-level experience catch-22! Gain valuable hands-on experience by working on real-world projects for businesses. 
+                            Build a strong portfolio showcasing your skills in practical application, making you a highly desirable candidate for potential employers.
+                        </li>
+                        <li className="text-gray-700">
+                            <strong>Learn from the Best: </strong>
+                            Get mentored by seasoned professionals in the software development industry. Gain invaluable insights and guidance that accelerate your learning curve. Ask questions, receive feedback,
+                            and learn from the expertise of those who have been there and done that.
+                        </li>
+                        <li className="text-gray-700">
+                            <strong>Stand Out from the Crowd: </strong>
+                            Don't let your resume get lost in the pile. Showcase the project experience you gain on Compile & Hire. 
+                            Highlight your capabilities in tackling real-world problems and using your knowledge to deliver results. 
+                            Make a lasting impression on potential employers and land your dream job.
+                        </li>
                     </ul>
                     </div>
                     <div>
                     <h3 className="text-2xl font-semibold mb-4">For Businesses</h3>
-                    <ul className="list-disc space-y-2 list-inside ">
-                        <li className="text-gray-700">Access a pool of pre-vetted talent.</li>
-                        <li className="text-gray-700">Cost-effective solutions for specific projects.</li>
-                        <li className="text-gray-700">Fresh perspectives and innovative ideas.</li>
+                    <ul className="list-disc space-y-2">
+                        <li className="text-gray-700">
+                            <strong>Leverage a Top-Tier Talent Pool:  </strong>
+                            Find promising computer science students and aspiring engineers ready to contribute to your projects. 
+                            We curate a talented pool of individuals, saving you time and resources on recruitment. 
+                            Leverage our network to find the perfect fit for your specific needs and skill requirements.
+                        </li>
+                        <li className="text-gray-700">
+                            <strong>Cost-Effective Solutions: </strong>
+                            Hire talent for specific projects without the commitment of a full-time employee. This allows you to scale your development team efficiently 
+                            and manage project budgets effectively. Need help with a short-term development task or a specific feature? Find the right talent on Compile & Hire 
+                            for a cost-effective solution.
+                        </li>
+                        <li className="text-gray-700">
+                            <strong>Fresh Perspectives and Innovation: </strong>
+                            Tap into the energy, creativity, and latest skillsets of the next generation of developers. 
+                            Gain access to new ideas and approaches that can benefit your projects. Compile & Hire connects you with young minds eager to learn and contribute, 
+                            bringing innovative solutions and fresh perspectives to your development efforts.
+                        </li>
                     </ul>
                     </div>
                 </div>
-                <p className="text-center text-gray-700 mt-8 pt-10">Together, we can build a thriving community of learners, innovators, and collaborators.</p>
-                <p className="text-center mt-4 pt-5">
-                    <a href="/" className="inline-flex items-center px-4 py-2 bg-blue-500 text-white font-bold rounded-md hover:bg-blue-700">Join Compile & Hire Today!</a>
-                </p>
-                </section>
+
+                <div className="text-center text-gray-700 mt-8">
+                    <p>Together, We Make a Difference: Compile & Hire fosters a thriving community where aspiring developers gain the practical experience they need to launch successful careers, and businesses find the talented individuals they seek to take their projects to the next level. We bridge the gap between education and opportunity, empowering both sides to achieve their goals.</p>
+                    <p className="mt-2">Join Compile & Hire today and unlock the potential of the future workforce!</p>
+                </div>
+                <div className="text-center flex items-center justify-center pt-5">
+                    <Link href="/" className=" px-4 py-2 bg-blue-500 text-white font-bold rounded-md hover:bg-blue-700">
+                        Join Compile & Hire Today!
+                    </Link>
+                </div >
+            </section>
         </div>
     );
 };
