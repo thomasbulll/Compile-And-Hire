971ceb15422f7b69658c0e2c01679136c7850ad2
Thomas Bull <tombull66@hotmail.com>
2024-06-10 16:56:53 +0100

    Improve choose register type page

--- app/auth/choose-register-type/page.tsx
+++ app/auth/choose-register-type/page.tsx
@@ -1,12 +1,30 @@
-'use client'
+"use client";
 
-import { ChooseRegisterTypeForm } from '@/components/auth/choose-register-type-form';
+import Link from "next/link"
 
-export default function ChooseRegisterTypePage() {
-    return (
-      <main className="main pt-36 flex justify-center items-center xl:flex-row flex-col gap-5">
-        <ChooseRegisterTypeForm />
-      </main>
-    );
+export default function ChooseRegisterType() {
+  return (
+    <div className="main flex justify-center items-center xl:flex-row flex-col gap-5">
+      <div className="grid grid-cols-2 overflow-hidden rounded-[0.5rem] border bg-background shadow w-[1500px] pt-30 h-[800px]">
+        <Link className="bg-zinc-900 hover:bg-stone-800" href="/auth/business-register">
+          <div className="relative hidden h-full flex-col p-10 text-white lg:flex dark:border-r">
+            <div className="absolute inset-0" />
+            <div className="relative z-20 flex items-center text-lg font-medium">
+            Business
+            </div>
+          </div>
+        </Link>
+        <Link className="bg-stone-900 hover:bg-zinc-800" href="/auth/student-register">
+          <div className="relative hidden h-full flex-col p-10 text-white lg:flex dark:border-r">
+              <div className="absolute inset-0" />
+              <div className="relative z-20 flex items-center text-lg font-medium">
+              Student
+              </div>
+          </div>
+        </Link>
+    </div>
+
+    </div>
+  )
   }
   
